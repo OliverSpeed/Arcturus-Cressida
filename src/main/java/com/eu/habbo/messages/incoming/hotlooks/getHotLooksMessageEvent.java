5f69164e2c9416b47940ff635556c68a2e36bcc0
brenoepic <59066707+brenoepics@users.noreply.github.com>
2022-05-11 00:25:13 -0300

    Update getHotLooksMessageEvent.java

--- src/main/java/com/eu/habbo/messages/incoming/hotlooks/getHotLooksMessageEvent.java
+++ src/main/java/com/eu/habbo/messages/incoming/hotlooks/getHotLooksMessageEvent.java
@@ -1,12 +1,13 @@
 package com.eu.habbo.messages.incoming.hotlooks;
 
+import com.eu.habbo.Emulator;
 import com.eu.habbo.messages.incoming.MessageHandler;
 import com.eu.habbo.messages.outgoing.hotlooks.hotLooksMessageComposer;
+import com.eu.habbo.messages.outgoing.users.HotLooksMessageComposer;
 
 public class getHotLooksMessageEvent extends MessageHandler {
     @Override
     public void handle() throws Exception {
-        int something = this.packet.readInt();
-        string
+        this.client.sendResponse(new HotLooksMessageComposer(Emulator.getGameEnvironment().getHabboManager().getHotlooks()));
     }
 }
