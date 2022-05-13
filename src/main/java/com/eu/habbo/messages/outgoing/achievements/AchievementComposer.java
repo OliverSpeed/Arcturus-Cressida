d4d900a4076eeddbf42c74e08c56639f66316b2b
Breno Andrade <59066707+brenoepics@users.noreply.github.com>
2022-05-13 03:43:19 -0300

    Update AchievementComposer.java

--- src/main/java/com/eu/habbo/messages/outgoing/achievements/AchievementComposer.java
+++ src/main/java/com/eu/habbo/messages/outgoing/achievements/AchievementComposer.java
@@ -20,40 +20,33 @@ public class AchievementComposer extends MessageComposer {
     @Override
     protected ServerMessage composeInternal() {
         this.response.init(Outgoing.achievementComposer);
+        
+            int achievementProgress = this.habbo.getHabboStats().getAchievementProgress(this.achievement);
+            AchievementLevel currentLevel = this.achievement.getLevelForProgress(achievementProgress);
+            AchievementLevel nextLevel = this.achievement.getNextLevel(currentLevel != null ? currentLevel.level : 0);
 
-        int achievementProgress;
-        AchievementLevel currentLevel;
-        AchievementLevel nextLevel;
+            if(currentLevel == null) {
+                currentLevel = this.achievement.firstLevel();
+            }
 
-        achievementProgress = this.habbo.getHabboStats().getAchievementProgress(this.achievement);
-        currentLevel = this.achievement.getLevelForProgress(achievementProgress);
-        nextLevel = this.achievement.getNextLevel(currentLevel != null ? currentLevel.level : 0);
+            if(nextLevel == null) {
+                nextLevel = currentLevel;
+            }
 
-        if (currentLevel != null && currentLevel.level == this.achievement.levels.size())
-            nextLevel = null;
-
-        int targetLevel = 1;
-
-        if (nextLevel != null)
-            targetLevel = nextLevel.level;
-
-        if (currentLevel != null && currentLevel.level == this.achievement.levels.size())
-            targetLevel = currentLevel.level;
-
-        this.response.appendInt(this.achievement.id); //ID
-        this.response.appendInt(targetLevel); //Target level
-        this.response.appendString("ACH_" + this.achievement.name + targetLevel); //Target badge code
-        this.response.appendInt(currentLevel != null ? currentLevel.progress : 0); //Last level progress needed
-        this.response.appendInt(nextLevel != null ? nextLevel.progress : 0); //Progress needed
-        this.response.appendInt(nextLevel != null ? nextLevel.rewardAmount : 0); //Reward amount
-        this.response.appendInt(nextLevel != null ? nextLevel.rewardType : 0); //Reward currency ID
-        this.response.appendInt(achievementProgress == -1 ? 0 : achievementProgress); //Current progress
-        this.response.appendBoolean(AchievementManager.hasAchieved(this.habbo, this.achievement)); //Achieved? (Current Progress == MaxLevel.Progress)
-        this.response.appendString(this.achievement.category.toString().toLowerCase()); //Category
-        this.response.appendString(""); //Empty, completly unused in client code
-        this.response.appendInt(this.achievement.levels.size()); //Count of total levels in this achievement
-        this.response.appendInt(0); //1 = Progressbar visible if the achievement is completed
-        this.response.appendShort(achievement.state); //state - 0 - disabled, 1 = enabled, 2 = archive
+            this.response.appendInt(this.achievement.id); //ID
+            this.response.appendInt(nextLevel.level); // next level
+            this.response.appendString("ACH_" + this.achievement.name + nextLevel.level); //Target badge code
+            this.response.appendInt(currentLevel.progress); //Last level progress needed
+            this.response.appendInt(nextLevel.progress); //Progress needed
+            this.response.appendInt(nextLevel.rewardAmount); //Reward amount
+            this.response.appendInt(nextLevel.rewardType); //Reward currency ID
+            this.response.appendInt(Math.max(achievementProgress, 0)); //Current progress
+            this.response.appendBoolean(currentLevel == nextLevel && achievementProgress >= nextLevel.progress); //Achieved? (Current Progress == MaxLevel.Progress)
+            this.response.appendString(this.achievement.category.name()); //Category
+            this.response.appendString(""); //Sub category
+            this.response.appendInt(this.achievement.levels.size()); //Count of total levels in this achievement
+            this.response.appendInt(currentLevel == nextLevel && achievementProgress >= nextLevel.progress ? 1 : 0); //1 = Progressbar visible if the achievement is completed
+            this.response.appendShort(this.achievement.state); //state - 0 - disabled, 1 = enabled, 2 = archive
         
         return this.response;
     }
